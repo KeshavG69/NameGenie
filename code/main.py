7c9ddc2542f878c86fc91333436ab10fe9563c60
Keshav Garg <98670495+KeshavG69@users.noreply.github.com>
2024-08-18 05:01:12 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,28 +1,22 @@
-import utils
-from transformers import AutoProcessor, AutoModelForCausalLM, AutoTokenizer
-import torch
+from utils import *
+
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Rename All the files in the directory')
-parser.add_argument('directory', type=str, help="Enter The Directory which files you want to rename" )
+parser = argparse.ArgumentParser(description="Rename All the files in the directory")
+parser.add_argument(
+    "directory", type=str, help="Enter The Directory which files you want to rename"
+)
+parser.add_argument(
+    "device", type=str, help="Enter The Device type i.e. 'cuda','mps','cpu'"
+)
+
 args = parser.parse_args()
 
-torch.set_default_device("cuda")
-
-image_model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/Florence-2-base", trust_remote_code=True
-).eval()
-image_processor = AutoProcessor.from_pretrained(
-    "microsoft/Florence-2-base", trust_remote_code=True
-).eval()
-
-text_model = AutoModelForCausalLM.from_pretrained(
-    "Qwen/Qwen1.5-1.8B-Chat", torch_dtype="auto", device_map="auto"
-)
-text_tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-1.8B-Chat")
 
 
-directory_path=args.directory
 
-utils.rename(directory_path)
+directory_path = args.directory
+device=args.device
+
+rename(directory_path,device)
